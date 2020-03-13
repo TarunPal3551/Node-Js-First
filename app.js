23a021f7914cbaca69cad115303d06597e539eac
Tarun <tarunplay3551@gmail.com>
2020-03-14 00:33:10 +0530

    3/14/2020

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const morgan=require('morgan');
 const bodyParser=require('body-parser')
+const mongoose=require('mongoose');
+
 const app=express();
 const productRoutes=require('./api/routes/products');
 const orderRoutes=require('./api/routes/orders');
+mongoose.connect('mongodb+srv://admin:<admin>@firstnode-ijo0j.mongodb.net/test?retryWrites=true&w=majority')
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended:false}));
 app.use(bodyParser.json());
